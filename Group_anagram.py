bda53c50cebfe9403ba2f8b72c3e111c4bf3b4b2
Jeevtesh Singh <50637812+jeevy222@users.noreply.github.com>
2020-02-11 16:46:36 -0500

    Add files via upload

---
+++ Group_anagram.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        dict = {}
+        for word in strs:
+            sortedword = "".join(sorted(word))
+            if sortedword not in dict:
+                dict[sortedword] = [word]
+            else:
+                dict[sortedword].append(word)
+
+        return dict.values()
+
